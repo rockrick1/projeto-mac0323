bcbf4958bff4af337c751f9af2cc4e19f02b7092
henrique cerquinho <henriquecerquinho@gmail.com>
2018-03-27 12:45:28 -0300

    esquema de paragrafo quase funcionando, linha apos o paragrafo sai zuada ultima palavra excedendo limites de colunas

--- pr1.c
+++ pr1.c
@@ -20,15 +20,13 @@ void write(char **words, int spaces[], int word_count) {
 
 /* le um arquivo e printa o texto justificado em col colunas */
 void **readFile(char *filename, int col) {
-	int i, j, k, p;
+	int i, j, k, p, EOL_count;
 	int word_count; /* quantidade de palavras na linha ate agora. reseta toda linha */
 	int excess; /* espaços em excesso no final da iteração */
 	char c;
-    char prev;
 	char *buf; /* buffer para palavra */
 	int cur_word_size; /* vetor com tamanho das palavras da linha para usar na formula */
-    int spaces[col/2]; /* vetor com espaços a serem inseridos entre cada palavra.
-	 			/// tamanho do vetor é num de palavras - 1*/
+	int spaces[col/2]; /* vetor com espaços a serem inseridos entre cada palavra. */
 	char **words; /* buffer para as linhas */
 
 	FILE *file;
@@ -51,51 +49,71 @@ void **readFile(char *filename, int col) {
 		strcpy(words[i], "");
 	}
 
-	prev = '\0';
+	EOL_count = 0;
 	word_count = 0;
 	cur_word_size = 0;
 
 	c = getc(file);
 	for (i = j = 0; c != EOF; j++) {
 		/* se nao for espaço nem newline, escreve no buffer */
-        if (c != ' ' && c != '\n' && c != EOF && c != '\r' && c != '\t') {
-            buf[i] = c;
+		if (c != ' ' && c != '\n' && c != EOF && c != '\r' && c != '\t') {
+			buf[i] = c;
 			cur_word_size++;
 			i++;
 			c = getc(file);
-        }
+		}
 		/* se nao for, faz varias coisas */
-        else {
+		else {
 			/* tira espaços ou tabs a mais, se tiverem seguidos etc */
-			while (c == ' ' || c == '\n' || c == EOF || c == '\r' || c == '\t')
+			while (c == ' ' || c == '\n' || c == EOF || c == '\r' || c == '\t') {
+				if (c == '\n')
+					EOL_count++;
 				c = getc(file);
+			}
 
 			buf[i] = '\0'; /* fecha o buffer como string */
 
-			if (c == prev && prev == '\n') { /* fim do paragrafo, sem ideias ainda */
-				continue;
+			/* ideia para fim do paragrafo, ta cagando algo que nao sei oq eh */
+			if (EOL_count >= 2) {
+				/* copia a ultima palavra */
+				strcpy(words[word_count], buf);
+				word_count++;
+
+				/* todos os espaços serão 1 entre as palavras */
+				for (k = 0; k < word_count - 1; k++)
+					spaces[k] = 1;
+				spaces[word_count - 1] = 0;
+				write(words, spaces, word_count);
+				printf("\n");
+
+				/* reseta spaces, words e EOL_count */
+				for (k = 0; k < col/2; k++)
+					spaces[k] = 1;
+				for (k = 0; k < word_count; k++)
+					strcpy(words[k], "");
+				j = 0;
 			}
 
 			/* se ultrapassou o limite de colunas */
-			if (j > col) {
+			else if (j > col) {
 				/* define os espaços a serem printados nessa linha */
-                excess = cur_word_size - (j - col) + 1; /* quantos espaços sobraram */
+				excess = cur_word_size - (j - col) + 1; /* quantos espaços sobraram */
 				p = word_count - 2;
-                for (k = 0; k < excess; k++) {
-                    spaces[p]++;
+				for (k = 0; k < excess; k++) {
+					spaces[p]++;
 					p--;
 					if (p < 0)
 						p = word_count - 2;
-                }
+				}
 				spaces[word_count - 1] = 0;
 				/***************************************************/
 
 				/* escreve as parada */
-                write(words, spaces, word_count);
+				write(words, spaces, word_count);
 
 				/* reseta spaces e words */
 				for (k = 0; k < col/2; k++)
-                	spaces[k] = 1;
+				spaces[k] = 1;
 				for (k = 0; k < word_count; k++) {
 					strcpy(words[k], "");
 				}
@@ -104,7 +122,7 @@ void **readFile(char *filename, int col) {
 				j = cur_word_size;
 				if (j == 0) word_count = 0;
 				else word_count = 1;
-            }
+			}
 
 			else {
 				strcpy(words[word_count], buf);
@@ -114,8 +132,8 @@ void **readFile(char *filename, int col) {
 			buf[0] = '\0'; /* reseta o buffer */
 			i = 0;
 			cur_word_size = 0;
-        }
-        prev = c; /* ignore pls*/
+			EOL_count = 0;
+		}
 	}
 
 	/* escreve a ultima linha i guess */
