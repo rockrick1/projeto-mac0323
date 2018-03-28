e996bd7d8869cfd4de2ff557f65b851be5d2e4c7
henrique cerquinho <henriquecerquinho@gmail.com>
2018-03-28 13:02:00 -0300

    codigo refatorado e paragrafo funcionando, só falta ultima linha

--- pr1.c
+++ pr1.c
@@ -55,73 +55,75 @@ void **readFile(char *filename, int col) {
 
 	c = getc(file);
 	for (i = j = 0; c != EOF; j++) {
-		/* se nao for espaço nem newline, escreve no buffer */
 		if (c != ' ' && c != '\n' && c != EOF && c != '\r' && c != '\t') {
 			buf[i] = c;
 			cur_word_size++;
 			i++;
 			c = getc(file);
 		}
-		/* se nao for, faz varias coisas */
+
 		else {
-			/* tira espaços ou tabs a mais, se tiverem seguidos etc */
+			buf[i] = '\0'; /* fecha o buffer como string */
+
 			while (c == ' ' || c == '\n' || c == EOF || c == '\r' || c == '\t') {
 				if (c == '\n')
 					EOL_count++;
 				c = getc(file);
 			}
 
-			buf[i] = '\0'; /* fecha o buffer como string */
+			if (EOL_count >= 2 || j > col) {
+				if (j <= col) { /* apenas excedeu EOL */
+					/* copia a ultima palavra */
+					strcpy(words[word_count], buf);
+					word_count++;
 
-			/* ideia para fim do paragrafo, ta cagando algo que nao sei oq eh */
-			if (EOL_count >= 2) {
-				/* copia a ultima palavra */
-				strcpy(words[word_count], buf);
-				word_count++;
-
-				/* todos os espaços serão 1 entre as palavras */
-				for (k = 0; k < word_count - 1; k++)
-					spaces[k] = 1;
-				spaces[word_count - 1] = 0;
-				write(words, spaces, word_count);
-				printf("\n");
-
-				/* reseta spaces, words e EOL_count */
-				for (k = 0; k < col/2; k++)
-					spaces[k] = 1;
-				for (k = 0; k < word_count; k++)
-					strcpy(words[k], "");
-				j = 0;
-			}
-
-			/* se ultrapassou o limite de colunas */
-			else if (j > col) {
-				/* define os espaços a serem printados nessa linha */
-				excess = cur_word_size - (j - col) + 1; /* quantos espaços sobraram */
-				p = word_count - 2;
-				for (k = 0; k < excess; k++) {
-					spaces[p]++;
-					p--;
-					if (p < 0)
+					/* todos os espaços serão 1 entre as palavras */
+					for (k = 0; k < word_count - 1; k++)
+						spaces[k] = 1;
+					spaces[word_count - 1] = 0;
+					write(words, spaces, word_count);
+					printf("\n");
+					word_count = 0;
+					j = -1;
+				}
+				else if (j > col) { /* apenas excedeu col */
+					/* define os espaços a serem printados nessa linha */
+					excess = cur_word_size - (j - col) + 1;
+					p = word_count - 2;
+					for (k = 0; k < excess; k++) {
+						spaces[p]++;
+						p--;
+						if (p < 0)
 						p = word_count - 2;
-				}
-				spaces[word_count - 1] = 0;
-				/***************************************************/
+					}
+					spaces[word_count - 1] = 0;
+					/***************************************************/
 
-				/* escreve as parada */
-				write(words, spaces, word_count);
+					/* escreve as parada */
+					write(words, spaces, word_count);
 
-				/* reseta spaces e words */
-				for (k = 0; k < col/2; k++)
-				spaces[k] = 1;
-				for (k = 0; k < word_count; k++) {
-					strcpy(words[k], "");
+					/* reseta spaces e words */
+					for (k = 0; k < col/2; k++)
+						spaces[k] = 1;
+					for (k = 0; k < word_count; k++) {
+						strcpy(words[k], "");
+					}
+					/* Se a ultima palavra (que excedeu col) também é o fim do
+					// paragrafo, printa ela sozinha e começa o role denovo */
+					if (EOL_count >= 2) {
+						printf("%s\n\n", buf);
+						word_count = 0;
+						j = -1;
+					}
+					/* senão, só copia a palavra que excedeu o limite
+					// e reseta word_count */
+					else {
+						strcpy(words[0], buf);
+						j = cur_word_size;
+						if (j == 0) word_count = 0;
+						else word_count = 1;
+					}
 				}
-				/* copia a palavra que excedeu o limite */
-				strcpy(words[0], buf);
-				j = cur_word_size;
-				if (j == 0) word_count = 0;
-				else word_count = 1;
 			}
 
 			else {
@@ -159,8 +161,6 @@ int main(int argc, char **argv) {
 	readFile(argv[1], atoi(argv[2]));
 	return 0;
 }
-
-
 
 
 
