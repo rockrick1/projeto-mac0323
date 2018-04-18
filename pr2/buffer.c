abc326671fd0220c561e90a22a0b204781c5f8fd
cerco <henriquecerquinho@gmail.com>
2018-04-18 09:40:52 -0300

    umas coisa ai

--- pr2/buffer.c
+++ pr2/buffer.c
@@ -7,24 +7,47 @@
   Create and return a new buffer that holds member elements of size
   member_size.
 */
-Buffer *buffer_create(size_t member_size);
+Buffer *buffer_create(size_t member_size) {
+    Buffer *B = (Buffer*) malloc(sizeof(Buffer));
+    B->member_size = member_size;
+    // B->buffer_size = ???
+    B->data = (char*) malloc(B->buffer_size * (B->member_size * sizeof(char)));
+    return B;
+}
 
 /*
   Destroy a buffer.
 */
-void buffer_destroy(Buffer *B);
+void buffer_destroy(Buffer *B) {
+    free(B->data);
+    free(B);
+}
 
 /*
   Reset buffer, eliminating contents.
 */
-void buffer_reset(Buffer *B);
+void buffer_reset(Buffer *B) {
+    B->data = NULL; // ?????
+}
 
 /*
   Return a valid pointer to the first free position of the
   buffer. This means that, if the space allocated is not enough, then
   the buffer size is increased and the contents are copied.
 */
-void *buffer_push_back(Buffer *B);
+void *buffer_push_back(Buffer *B) {
+    int i, size = B->member_size * B->buffer_size;
+    for (i = 0; i < size; i++)
+        if (B->data[i] == NULL)
+            break;
+    // excederá o limite, realoca o buffer
+    if (i == size) {
+        B->data = realloc(B->data, 2*sizeof(B->data)); // might work idk
+        // idk either, a função ser void nao ajuda tambem
+        return (char*) B->data[size];
+    }
+    return (char*) B->data[i];
+}
 
 /*
   Read a line (i.e., reads up to a newline '\n' character or the
@@ -34,4 +57,10 @@ void *buffer_push_back(Buffer *B);
   Returns the number of characters read; in particular, returns ZERO
   if end-of-file is reached before any characters are read.
 */
-int read_line(FILE *input, Buffer *B);
+int read_line(FILE *input, Buffer *B) {
+    char c = getc(input);
+    while (c != '\n') {
+        buffer_push_char(B, c);
+        c = getc(input);
+    }
+}
--- pr2/center.c
+++ pr2/center.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <ctype.h>
 #include "buffer.h"
 
 int main(int argc, char **argv) {
+    // argv[1] = arquivo de entrada
+    // argv[2] = arquivo de saida
+    // argv[3] = numero de colunas
 	return 0;
 }
