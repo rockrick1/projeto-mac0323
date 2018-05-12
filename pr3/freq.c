e8085a0c0d32390b411b897abde55d8ad41c66da
DarknessCatt <matheus.limacunha@hotmail.com>
2018-05-12 12:33:45 -0300

    Freq agora ordena!

--- pr3/freq.c
+++ pr3/freq.c
@@ -6,17 +6,16 @@
 int main(int argc, char **argv) {
     SymbolTable ST = stable_create(17);
 
-    // EntryData d;
-    // d.i = 12;
-
 	stable_insert(ST, "0");
     stable_insert(ST, "9");
     stable_insert(ST, "abc");
+    stable_insert(ST, "aab");
+    stable_insert(ST, "zxc");
     stable_insert(ST, "89wrtu9b");
     stable_insert(ST, "89wrb");
-    // lista_print(ST->pos[0]);
-    // lista_insert(ST->pos[0], "abc", d);
-    // lista_print(ST->pos[0]);
+
+    //Coloca toda a lista em a
+
     char *a[ST->n];
     int b = 0;
     for(int i = 0; i<ST->m; i++){
@@ -27,27 +26,46 @@ int main(int argc, char **argv) {
     printf("\n");
     int max = -1;
 
+    //Acha o valor maximo
+
     for(int i = 0; i<ST->n; i++){
         if((int)(strlen(a[i]))>max)
             max = strlen(a[i]);
     }
 
-    //ALGUEM ORDENA AQUI PLS
+    int j;
+    int diff;
 
-    int diff = 0;
+    //Ordena por insertion sort
+
+    for(int i = 0; i<ST->n; i++){
+
+        j = i;
+
+        while(j>0){
+            diff = strcmp(a[j],a[j-1]);
+            if(diff>0)
+                break;
+            else{
+                char *t = a[j];
+                a[j] = a[j-1];
+                a[j-1] = t;
+                j--;
+            }
+        }
+    }
+
+    //Imprime tudo com o numero de espaços bonito
 
     for(int i = 0; i<ST->n; i++){
         diff = max - (int)(strlen(a[i]));
         printf("%s ", a[i]);
         for(int j = 0; j<diff; j++)
             printf(" ");
-        printf("%d\n", 10);//<------------------------ get aqui
+        printf("%d\n", stable_find(ST,a[i])->i);
     }
 
     printf("\n");
-    // lista_insert(ST->pos[0], "oof", d);
-    // lista_print(ST->pos[0]);
-    printf("kek\n");
     stable_destroy(ST);
 
     return 0;
