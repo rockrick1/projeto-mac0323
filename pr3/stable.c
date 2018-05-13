ab969f921bfcd71269c24b580841be5c59a9b293
henrique cerquinho <henriquecerquinho@gmail.com>
2018-05-13 12:36:33 -0300

    comentarios

--- pr3/stable.c
+++ pr3/stable.c
@@ -24,7 +24,7 @@ static int hash(const char* chave, int m) {
 
 SymbolTable stable_create(int m) {
 
-    SymbolTable ST = malloc(sizeof(stable_s)); // como faz isso pls halp
+    SymbolTable ST = malloc(sizeof(stable_s));
     ST->pos = malloc(m * sizeof(Lista));
     for (int i = 0; i < m; i++)
         ST->pos[i] = lista_create();
@@ -48,11 +48,13 @@ InsertionResult stable_insert(SymbolTable table, const char *key) {
     InsertionResult result;
     int idx = hash(key, table->m);
 
+    // começa com val 1 caso seja uma nova palavra
     EntryData val;
     val.i = 1;
 
     result = lista_insert(table->pos[idx], key, val);
 
+    // se for nova, atualiza o tamanho da ST
     if (result.new != 0)
         table->n++;
 
@@ -63,10 +65,12 @@ EntryData *stable_find(SymbolTable table, const char *key) {
     EntryData *data;
     int idx = hash(key, table->m);
 
+    // se nao tiver nenhuma chave com esse hash, retorna null
     if (table->pos[idx]->root == NULL) return NULL;
 
     Node *p = table->pos[idx]->root;
 
+    // percorre a lista com o hash da chave
     while (p != NULL) {
         if (strcmp(key, p->key) == 0) {
             data = &p->val;
@@ -74,16 +78,13 @@ EntryData *stable_find(SymbolTable table, const char *key) {
         }
         p = p->next;
     }
+    // retorna null se nao está lá
     return NULL;
 }
 
 int stable_visit(SymbolTable table, int (*visit)(const char *key, EntryData *data)) {
 
-    // se tirar isso ainda funciona do jeito que eu testei, mas talvez de
-    // testa de outro jeito precise disso, então acho bom deixar
-    /*****************/
     visit = print_node;
-    /*****************/
 
     for (int i = 0; i < table->m; i++) {
 
