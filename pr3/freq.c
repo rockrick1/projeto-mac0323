beac4b2ec279af7e724a06e4048da2555054e143
Anb <gabriel.massukado@hotmail.com>
2018-05-13 17:04:21 -0300

    Sent :)

--- pr3/freq.c
+++ pr3/freq.c
@@ -70,19 +70,24 @@ int main(int argc, char **argv) {
 
     printf("\n");
 
+    //acha o valor maximo de espaços
+
     int max = -1;
+
+    for (int i = 0; i < ST->n; i++) {
+    	if ((int)(strlen(a[i])) > max)
+            max = strlen(a[i]);
+    }
+
     int j;
     int diff;
 
 
-    //Ordena por insertion sort enquanto procura o valor maximo
+    //Ordena por insertion sort
 
     for(int i = 0; i<ST->n; i++){
 
         j = i;
-
-        if((int)(strlen(a[i]))>max)
-            max = strlen(a[i]);
 
         while(j>0){
             diff = strcmp(a[j],a[j-1]);
@@ -103,8 +108,9 @@ int main(int argc, char **argv) {
     for(int i = 0; i<ST->n; i++){
         diff = max - (int)(strlen(a[i]));
         printf("%s ", a[i]);
-        for(int j = 0; j<diff; j++)
+        for(int j = 0; j<diff; j++) {
             printf(" ");
+        }
         printf("%d\n", stable_find(ST,a[i])->i);
     }
 
