7abca528056b261d35be22550fc49469de2d2c3d
DarknessCatt <matheus.limacunha@hotmail.com>
2018-06-01 16:35:07 -0300

    parser com varios ifs

--- pr4/parser.c
+++ pr4/parser.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int parse(const char *s, SymbolTable alias_table, Instruction **instr, const char **errptr){
 
@@ -47,7 +48,7 @@ int parse(const char *s, SymbolTable alias_table, Instruction **instr, const cha
 
 		while(s[k]==' ') k++; //acha o inicio da palavra
 		i = k;
-		while(s[i]!=' ' || s[i]!='\n' || s[i]!=EOF) i++; // acha o fim da palavra
+		while(s[i]!=' ' || s[i]!='\n' || s[i]!='*' || s[i]!=EOF) i++; // acha o fim da palavra
 
 		l = i-k;
 		char t_op[l];
@@ -68,19 +69,64 @@ int parse(const char *s, SymbolTable alias_table, Instruction **instr, const cha
 
 	for(int z = 0; z < 3; z++){
 
-		if(op->opd_types[z] == OP_NONE) break; //ele não precisa mais de argumentos
-
 		while(s[k]==' ') k++; //acha o inicio do argumento
 		i = k;
-		while(s[i]!=',' || s[i]!=' ' || s[i]!='\n' || s[i]!=EOF) i++; // acha o fim do argumento
+		while(s[i]!=',' || s[i]!=' ' || s[i]!='\n' || s[i]!='*' || s[i]!=EOF) i++; // acha o fim do argumento
 
 		l = i-k;
 
-		char *t_opd[l];
+		if(op->opd_types[z] == OP_NONE && l != 0){ //ele não precisa mais de argumentos, mas achou um
+			//Erro legal aqui
+		}
+
+		const char *t_opd[l];
 		for(int j = 0; j < l; j++, k++) t_opd[j] = s[k];
 
-		//opds[z] = operand_create o formato correto... Alguem sabe como fazer isso?
-		// 			eu pensei que teria que dar algum switch case no opd_types of no t_opd
+		int num = 0; //valor legal do operando ( caso necessario )
+
+		if(t_opd[0]=='$'){
+			if(op->opd_types[z]==REGISTER){
+				if(l<2){
+					//Erro legal aqui ( se l<2, quer dizer que ele só mandou '$' de argumento )
+				}
+				opds[z] = operand_create_register((unsigned char)strtol(t_opd[1],t_opd[l-1],10));
+			}
+			else{
+				//Erro legal aqui
+			}
+		}
+
+
+		else if(isdigit(t_opd[0])){ // então deve ser um numero, né?
+			if(op->opd_types[z] == NUMBER_TYPE){
+				opds[z] = operand_create_number(strtoll(t_opd[1],t_opd[l-1],10));
+			}
+			else{
+				//Erro legal aqui
+			}
+		}
+
+
+		else{ //é uma string, fudeu
+			EntryData *test = stable_find(alias_table,t_opd);
+
+			if(test == NULL){ // então é uma string
+				if(op->opd_types[z] == STRING){
+					opds[z] = operand_create_string(t_opd);
+				}
+				else{
+				//Erro legal aqui
+				}
+			}
+			else{ // então é um label
+				if(op->opd_types[z] == LABEL){
+					opds[z] = operand_create_label(t_opd);
+				}
+				else{
+				//Erro legal aqui
+				}
+			}
+		}
 
 	}
 
