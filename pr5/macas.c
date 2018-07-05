387975f08341c4b364e15ee8c4647b1722f4d653
DarknessCatt <matheus.limacunha@hotmail.com>
2018-07-05 15:12:58 -0300

    remoção de demasiados comentarios

--- pr5/macas.c
+++ pr5/macas.c
@@ -11,17 +11,11 @@
 int assemble(const char *filename, FILE *input, FILE *output) {
     FILE *getlabel = input;
 
-    //printf("Dei file input\n");
     Buffer *B = buffer_create(sizeof(char));
-    //printf("Criei o buffer\n");
     SymbolTable alias_table = stable_create(29);
-    //printf("Criei a table\n");
     Instruction *instr = NULL;
-    //printf("Criei a Instruction\n");
 
     const char *errptr;
-
-    //printf("\nVou procurar labels.\n");
 
     int pos = 1; //linha que estamos
     while(read_line(getlabel, B) != 0){
@@ -46,41 +40,28 @@ int assemble(const char *filename, FILE *input, FILE *output) {
 
         l = e - s;
 
-        //printf("Achei uma palavra de tamanho %d\n",l);
-
         if(l>0){
 
             char *word = malloc(sizeof(char)*(l+1));
             for(int i = 0; i<l; i++, s++) word[i] = line[s];
             word[l] = '\0';
-            //printf("A palavra é %s\n",word);
 
             const Operator *test;
             test = optable_find(word);
 
             if(test == NULL){
 
-                //printf("Acredito que a palavra seja um label.\n");
-
-                //printf("Mas a pergunta é, ele é um label valido? ");
-
                 if(!isalpha(word[0]) && word[0] != '_'){
                     errptr = &line[s];
                     set_error_msg("Invalid label name.");
-
-                    //Algo?
                 }
 
                 for(int j = 1; j<l; j++){
                     if( !isalpha(word[j]) && word[0] != '_' && !isdigit(word[j]) ){
                         errptr = &line[s];
                         set_error_msg("Invalid label name.");
-
-                        //Algo?
                     }
                 }
-
-                //printf("Sim!\n");
 
                 while(isspace(line[s])){
                     if(line[s]=='*' || line[s] == EOF ||line[s] == '\n') break;
@@ -88,9 +69,7 @@ int assemble(const char *filename, FILE *input, FILE *output) {
                 }
                 e = s+1;
 
-                //Verifica se é um IS, que cuidamos depois
                 if(line[s] != 'I' || line[e] != 'S'){
-                    //printf("Parece ser um label de endereço.\n");
 
                     InsertionResult result;
                     result = stable_insert(alias_table, word);
@@ -98,16 +77,12 @@ int assemble(const char *filename, FILE *input, FILE *output) {
                     if(result.new == 0){
                          errptr = &line[s];
                          set_error_msg("This label already exists");
-
-                         //Algo?
                     }
 
-                    //Cria um novo operando e coloca pos como valor dele
                     Operand *op = operand_create_label(word);
                     stable_find(alias_table, word)->opd = op;
                 }
                 else{
-                    //printf("Parece ser um label de atribuição.\n");
 
                     InsertionResult result;
                     result = stable_insert(alias_table, word);
@@ -115,38 +90,25 @@ int assemble(const char *filename, FILE *input, FILE *output) {
                     if(result.new == 0){
                         errptr = &line[s];
                         set_error_msg("This label already exists");
-
-                        //Algo?
                     }
                 }
             }
-            else{
-                //printf("Na verdade era um operator, continuando.\n");
-            }
         }
 
-        //printf("\n");
         pos++;
     }
     
-
-    //printf("Procurei em %d linhas.\n",pos);
-   //printf("Acredito ter achado todas as labels.\n");
-
-    set_prog_name("parse_test");
+    set_prog_name(filename);
 
     // passa por todas as linhas do codigo
     for (int line = 1;; line++) {
 
-    	//printf("\nDentro do for, linha %d\n",line);
-        // le a linha de codigo e poe no buffer
         if(read_line(input, B) == 0)
         	break;
 
         char *data = (char*)B->data;
 
         if (parse(data, alias_table, &instr, &errptr) == 0) {
-            printf("deu ruim, to vazando\n");
             print_error_msg(NULL);
             break;
         }
--- pr5/parser.c
+++ pr5/parser.c
@@ -8,17 +8,12 @@
 
 int parse(const char *s, SymbolTable alias_table, Instruction **instr, const char **errptr){
 
-	//test printf("\nEntrei no parse!\n");
-
-	//Assumo que o buffer tira todos os espaços antes do começo da palavra
-
 	int k = 0; //indice do começo da palavra
 	int i = 0; //indice que percorre s / indice do fim da palavra
 	int l = 0; //tamanho da palavra
 
 	const Operator *op;
 	Operand *opds[3];
-
 
 	//acha o fim da palavra
 	while(!isspace(s[i])) {
@@ -27,7 +22,7 @@ int parse(const char *s, SymbolTable alias_table, Instruction **instr, const cha
 	}
 
 	if (i == 0 && (s[i] == '\n' || s[i] == '*' || s[i] == EOF)) {
-		//test printf("Achei uma linha sem codigo, vou voltar.\n");
+		//linha sem codigo, voltar
 		return 1;
 	}
 
@@ -35,17 +30,9 @@ int parse(const char *s, SymbolTable alias_table, Instruction **instr, const cha
 
 	char label[l + 1];
 
-	//Copia a palavra se ela existir
 	if(l>0){
-		//test printf("Preparando para copiar a palavra com %d chars.\n",l);
-
 		for (int c = 0; c < l; c++, k++) label[c] = s[k];
 		label[l] = '\0';
-
-		//test printf("Palavra copiada com sucesso, tamanho %d .\n",l);
-	}
-	else{
-		//test printf("Encontrei uma palavra vazia.\n");
 	}
 
 
@@ -55,49 +42,17 @@ int parse(const char *s, SymbolTable alias_table, Instruction **instr, const cha
 
 	op = optable_find(label);
 
-	//Se op != null, na verdade encontramos um Operator
-	if(l>0 && op == NULL) {
-
-		//test printf("label: %s\n",label);
-
-	}
-	else { //é um OP então, olha que legal
-		//test printf("Acredito que a palavra seja um operator.\n");
-
-		// não sei se precisa de algo aqui TBH
-		// seria para deixar um o op guardado,
-		// mas acho que da para usar o op que a gente checa o null
-
-		//talvez desalocar o tlabel ou algo assim?
-
-	}
-
 	if(label !=  NULL) { //então temos que procurar o op
-		//test printf("\nVou começar a procurar o operator!\n");
 
-		while(isspace(s[k])) k++; //acha o inicio da palavra
-		//test printf("Achei o começo da palavra.\n");
+		while(isspace(s[k])) k++;
 		i = k;
-		// acha o fim da palavra
 		while(!isspace(s[i]) && s[i] != '\0') i++;
-		//test printf("Achei o fim da palavra.\n");
-		/*
-		if(s[i]=='\n' || s[i]=='*' || s[i]==EOF){
-			errptr = &s[k];							Acho que o teste abaixo já resolve esse caso,
-			set_error_msg("Missing Operator."); 	mas achei melhor deixar ele aqui por enquanto.
-			return 0; 								Vai que.
-		}
-		*/
 
-		l = i-k;
-		
+		l = i-k;	
 
-		//test printf("Preparando para copiar a palavra com %d chars.\n",l);
 		char t_op[l + 1];
 		for (int c = 0; c < l; c++, k++) t_op[c] = s[k];
 		t_op[l] = '\0';
-		//test printf("operator: %s\n", t_op);
-		//test printf("Palavra copiada com sucesso.\n");
 
 		op = optable_find(t_op);
 
@@ -109,12 +64,6 @@ int parse(const char *s, SymbolTable alias_table, Instruction **instr, const cha
 
 	}
 
-	//test printf("\nIndo pegar os operands.\n");
-
-	//Deem uma olhada no optable.c
-	//Basicamente cada operator tem um vertor com o que ele precisa.
-	//Se não precisar de argumentos, ele fala OP_NONE, assim:
-
 	// identifica quantos operandos sao necessarios
 	int operands = 3;
 	if (op->opd_types[2] == OP_NONE){ operands = 2; opds[2] = NULL; }
@@ -123,17 +72,12 @@ int parse(const char *s, SymbolTable alias_table, Instruction **instr, const cha
 
 	for(int z = 0; z < operands; z++){
 
-		//test printf("\n\nVou procurar o operand %d!\n",z);
-
 		k = i;
 		while(isspace(s[k])) k++; //acha o inicio do argumento
-		//test printf("Achei o inicio do operand.\n");
 		i = k;
-		// acha o fim do argumento
 		while(!isspace(s[i]) && s[i] != ',' && s[i] != '\0' && s[i] != EOF) i++;
 
 		l = i-k;
-		//test printf("Achei o fim do operand, de tamanho %d.\n",l);
 
 		if(op->opd_types[z] == OP_NONE && l != 0){ //ele não precisa mais de argumentos, mas achou um
 			*errptr = &s[k];
@@ -141,20 +85,11 @@ int parse(const char *s, SymbolTable alias_table, Instruction **instr, const cha
 			return 0;
 		}
 
-		//test printf("Vou começar a copiar esse operand.\n");
-
 		char t_opd[l + 1];
 		for (int c = 0; c < l; c++, k++) t_opd[c] = s[k];
 		t_opd[l] = '\0';
-		//test printf("opd: %s\n", t_opd);
-
-		//test printf("Operand copiado com sucesso.\n");
-
-
-		//test printf("Hora de verificar que tipo de Operand é esse!\n");
 
 		if(t_opd[0] == '$') {
-			//test printf("Parece que ele é um registrador!\n");
 
 			// aqui fazemos uma operação bitwise para comparar os possiveis
 			// tipos do operando com os aceitados pelo operador
@@ -168,18 +103,16 @@ int parse(const char *s, SymbolTable alias_table, Instruction **instr, const cha
 					set_error_msg("Missing register number.");
 					return 0;
 				}
-				//test printf("Vou calcular o numero do registrador.\n");
+				//calcular o numero do registrador
 				int num = 0;
 				for(int j = 1; j<l; j++){
-					//test printf("somando: %c\n",t_opd[j]);
 					if(!isdigit(t_opd[j])){ //evitar algo do tipo "$1a"
 						*errptr = &s[k];
 						set_error_msg("Invalid register number.");
 						return 0;
 					}
-					num = num*10 + (t_opd[j] - '0'); //se fosse float, essa operação teria menos erro ;D
+					num = num*10 + (t_opd[j] - '0');
 				}
-				//test printf("Calculei: %d\n",num);
 				opds[z] = operand_create_register((unsigned char)num);
 			}
 			else {
@@ -215,16 +148,14 @@ int parse(const char *s, SymbolTable alias_table, Instruction **instr, const cha
 			}
 		}
 
-
-		else { //é uma string, fudeu
-			//test printf("Ele deve ser uma string ou um label.\n");
+		else { //é uma string
 			EntryData *test = stable_find(alias_table, t_opd);
 
 			if(test == NULL){ // então é uma string
 				int num = -1;
 
 				if(l>1 && l<4 && t_opd[0] == 'r'){ //nenhum registrador especial
-												  //com menos de 2 e mais de 4 chars
+												   //com menos de 2 e mais de 4 chars
 					if(l == 3 && strcmp(t_opd,"rSP")){
 						num = 253;
 					}
@@ -261,8 +192,7 @@ int parse(const char *s, SymbolTable alias_table, Instruction **instr, const cha
 					return 0;
 				}
 			}
-			else{ // então é um label
-				//test printf("Parece que é um label de tipo %x \n",test->opd->type);
+			else{ // é um label
 				if(op->opd_types[z] & test->opd->type) { //verifica para o que a label aponta de verdade
 					//cria novo operador caso a caso do label
 					if(test->opd->type & REGISTER)
@@ -284,21 +214,9 @@ int parse(const char *s, SymbolTable alias_table, Instruction **instr, const cha
 		i++;
 	}
 
-	//test printf("\nAchei todos os operands.\n");
-
-	//test printf("Vou criar a nova instrução.\n");
 	Instruction *new = instr_create(label,op,opds);	//Cria a instruction nova
-	//test printf("Vou fazer ela apontar para o começo da lista.\n");
 	new->next = *instr;								//Faz ela apontar para a instrução anterior
-	//test printf("Vou colocar ela no começo da lista.\n");
 	*instr = new;									//Agora ela está no topo
-	//test printf("Pronto! Tudo parece ter sido feito!\n");
-
-
-
-
-	// A gente precisa dar frees aqui etc?
-
 
 	return 1;
 }
